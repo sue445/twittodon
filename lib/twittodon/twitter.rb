2126a24108766b5e5a1768252c410a124c97c1e2
sue445 <sue445@sue445.net>
2017-05-23 01:17:28 +0900

    Refactor

--- lib/twittodon/twitter.rb
+++ lib/twittodon/twitter.rb
@@ -19,7 +19,10 @@ module Twittodon
     # @param tweet [Twitter:Tweet]
     # @return [String]
     def self.expand_urls_text(tweet)
-      tweet.uris.reverse.each_with_object(tweet.text.dup) do |uri, expanded|
+      text = tweet.text.dup
+      return text unless tweet.uris?
+
+      tweet.uris.reverse.each_with_object(text) do |uri, expanded|
         pos1 = uri.indices[0]
         pos2 = uri.indices[1]
         expanded[pos1, pos2] = uri.expanded_url
@@ -30,7 +33,10 @@ module Twittodon
     # @param text [String]
     # @return [String]
     def self.remove_media_urls_in_tweet(tweet, text)
-      tweet.media.each_with_object(text.dup) do |media, t|
+      text = text.dup
+      return text unless tweet.media?
+
+      tweet.media.each_with_object(text) do |media, t|
         t.gsub!(media.url, "")
         t.strip!
       end
